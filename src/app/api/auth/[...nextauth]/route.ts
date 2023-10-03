dca323332547e4a0544730b9de2d915c73816a39
NalinduMelaka <nalindumelaka@outlook.com>
2023-10-04 03:27:10 +0530

    d

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -67,7 +67,7 @@ export const authOptions: AuthOptions = {
         signIn: '/auth/signin',
         signOut: '/auth/signout',
     },
-    secret: process.NEXTAUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     session: {
         strategy: 'jwt',
         maxAge: 30 * 24 * 60 * 60,
