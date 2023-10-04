f598ccd7cb6592e804fef3803557e5df50ba664d
Nalindu Melaka Vitharana <nalindumelaka@outlook.com>
2023-10-04 19:00:55 +0530

    Update PDFComponent.tsx

--- src/app/components/PDFComponent.tsx
+++ src/app/components/PDFComponent.tsx
@@ -8,7 +8,7 @@ const prisma = new PrismaClient();
 
 const PDFComponent = async () => {
   const fetchPDFs = async () => {
-    const pdfs = await prisma.pdfs.findMany();
+    const pdfs = await prisma.upload.findMany();
     return pdfs;
   };
 
@@ -28,11 +28,11 @@ const PDFComponent = async () => {
             pdfs.map(pdf => (
               <tr key={pdf.id}>
                 <td className="py-2 px-4 border-b">
-                  <Link href={`/pdf/${pdf.id}`} target="_blank" rel="noreferrer" className="text-blue-500 hover:underline">
-                    {pdf.id}
+                  <Link href={`/pdf/${pdf.filename}`} target="_blank" rel="noreferrer" className="text-blue-500 hover:underline">
+                    {pdf.filename}
                   </Link>
                 </td>
-                <td className='border-b'>{pdf.title}</td>
+                <td className='border-b'>{pdf.filename}</td>
               </tr>
             ))
           )}
